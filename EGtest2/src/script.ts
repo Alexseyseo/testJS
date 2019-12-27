7177a9189323ad5c21fe6b78235beb256b0b49c8
Alexsey Suprun <alexsey.seo@gmail.com>
2019-12-27 07:42:19 +0300

    WiP EGTest2. wip base structure class

--- EGtest2/src/script.ts
+++ EGtest2/src/script.ts
@@ -1,3 +1,4 @@
+'use strict';
 // Задание
 // Используя предоставленный тестовый сервер, написать класс, получающий по
 // отдельности все части каталога игр, а именно: категории, провайдеры, ограничения по странам, ограничения по валютам, игры (документация по api встроена в сервер).
@@ -13,19 +14,78 @@
 document.querySelectorAll('a').forEach((element) => {
     element.onclick = async (event: any) => {
         event.preventDefault();
+        getData.makeRequest(event.target.pathname, 1).then((response: PromiseResponse) => {
+            getData.displayInformation(response);
+            console.log('result', response);
+        });
+    }
+});
+
+interface PromiseResponse {
+    dataResponse: {} | [];
+    url: string;
+    countRequest: number;
+}
+
+class getData {
+    static categories() {
+        this.makeRequest('/GetCategories', 1).then((response: PromiseResponse) => {
+            this.displayInformation(response);
+            console.log('res', response);
+        });
+    }
+
+    static merchants() {
+        this.makeRequest('/GetMerchants', 1).then((response: PromiseResponse) => {
+            this.displayInformation(response);
+            console.log('res', response);
+        });
+    }
+
+    static countriesRestrictions() {
+        this.makeRequest('/GetCountriesRestrictions', 1).then((response: PromiseResponse) => {
+            this.displayInformation(response);
+            console.log('res', response);
+        });
+    }
+
+    static merchantsCurrencies() {
+        this.makeRequest('/GetMerchantsCurrencies', 1).then((response: PromiseResponse) => {
+            this.displayInformation(response);
+            console.log('res', response);
+        });
+    }
+
+    static async makeRequest(url: string, countRequest: number, options: object = {}): Promise<PromiseResponse> {
         try {
-            const url = event.target.pathname;
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw new Error(`Ошибка сервера.`);
+            const request = await fetch(url, options);
+            if (!request.ok) {
+                throw new Error(`Ошибка запроса сервера.`);
             }
-            const myJson = await response.json();
-            if (typeof myJson === 'string') {
-                console.log(JSON.parse(myJson));
+            const dataResponse = await request.json();
+            if (typeof dataResponse === 'string') {
+                return {
+                    dataResponse: JSON.parse(dataResponse),
+                    url,
+                    countRequest,
+                };
             }
-            console.log(myJson);
+            return {
+                dataResponse,
+                url,
+                countRequest,
+            };
         } catch (error) {
             console.log(error.message);
         }
+        return await this.makeRequest(url, ++countRequest);
     }
-});
+
+    static displayInformation(dataResponse: PromiseResponse) {
+        const blockInfo = document.createElement("div");
+        blockInfo.innerHTML = `Запрос: ${dataResponse.url}. Количество сделанных запросов: ${dataResponse.countRequest}.`;
+        document.body.append(blockInfo);
+    }
+}
+
+getData.categories();
