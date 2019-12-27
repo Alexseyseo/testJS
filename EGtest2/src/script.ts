50035fa72dd0079c7847bae25999a7490ab973e4
Alexsey Suprun <alexsey.seo@gmail.com>
2019-12-27 09:45:35 +0300

    WiP EGTest2. Complete promise work. wip displayed info

--- EGtest2/src/script.ts
+++ EGtest2/src/script.ts
@@ -11,54 +11,74 @@
 // осуществлять повторную попытку.
 //
 // После успешного завершения всех запросов вывести сообщение об этом, а также распределение (количество) игр по провайдерам и категориям.
-document.querySelectorAll('a').forEach((element) => {
-    element.onclick = async (event: any) => {
-        event.preventDefault();
-        getData.makeRequest(event.target.pathname, 1).then((response: PromiseResponse) => {
-            getData.displayInformation(response);
-            console.log('result', response);
-        });
-    }
-});
-
 interface PromiseResponse {
-    dataResponse: {} | [];
+    dataResponse: any;
     url: string;
     countRequest: number;
 }
 
 class getData {
+    static async games() {
+        let games: any[] = [];
+        let params = {
+            limit: 50,
+            start: 0,
+            sort: 'id',
+        };
+
+        await this.makeRequest('/GetGamesCount', 1).then((response: PromiseResponse) => {
+            this.paintLoading();
+            return response.dataResponse.count;
+        }).then((gamesCount) => {
+            let currentRequestIndex = 1;
+            for (let receivedGames = 0; receivedGames < gamesCount; receivedGames += 50) {
+                params.start = receivedGames;
+                let request = this.makeRequest('/GetGames', 1, params).then((response: PromiseResponse) => {
+                    this.paintLoading(currentRequestIndex++, games.length);
+                    return response;
+                });
+                games.push(request);
+            }
+        });
+
+        let results = await Promise.all(games);
+
+        // После успешного завершения всех запросов вывести сообщение об этом, а также распределение (количество) игр по провайдерам и категориям.
+        // TODO вывод в DOM информации об успешном завершении
+        console.log('all complete', results);
+    }
+
     static categories() {
         this.makeRequest('/GetCategories', 1).then((response: PromiseResponse) => {
             this.displayInformation(response);
-            console.log('res', response);
         });
     }
 
     static merchants() {
         this.makeRequest('/GetMerchants', 1).then((response: PromiseResponse) => {
             this.displayInformation(response);
-            console.log('res', response);
         });
     }
 
     static countriesRestrictions() {
         this.makeRequest('/GetCountriesRestrictions', 1).then((response: PromiseResponse) => {
             this.displayInformation(response);
-            console.log('res', response);
         });
     }
 
     static merchantsCurrencies() {
         this.makeRequest('/GetMerchantsCurrencies', 1).then((response: PromiseResponse) => {
             this.displayInformation(response);
-            console.log('res', response);
         });
     }
 
-    static async makeRequest(url: string, countRequest: number, options: object = {}): Promise<PromiseResponse> {
+    static async makeRequest(url: string, countRequest: number, params: object = null): Promise<PromiseResponse> {
         try {
-            const request = await fetch(url, options);
+            if (params) {
+                // @ts-ignore
+                url = url + `?${Object.keys(params).map(key => key + '=' + params[key]).join('&')}`;
+            }
+            const request = await fetch(url);
             if (!request.ok) {
                 throw new Error(`Ошибка запроса сервера.`);
             }
@@ -78,14 +98,60 @@ class getData {
         } catch (error) {
             console.log(error.message);
         }
+        // В случае получения ошибки получения данных выводить сообщение об ошибке и
+        // осуществлять повторную попытку.
+        // TODO выводить сообщение об ошибке в DOM
         return await this.makeRequest(url, ++countRequest);
     }
 
     static displayInformation(dataResponse: PromiseResponse) {
         const blockInfo = document.createElement("div");
+        console.log(dataResponse.url, dataResponse.dataResponse);
         blockInfo.innerHTML = `Запрос: ${dataResponse.url}. Количество сделанных запросов: ${dataResponse.countRequest}.`;
         document.body.append(blockInfo);
     }
+
+    private static paintLoading(current: number = 0, all: number = 100) {
+        // В процессе выводить в DOM количество сделанных и оставшихся запросов, а также
+        // процент выполнения.
+        let box = document.getElementById('loadingGames');
+        if (box) {
+            let range = document.getElementById('rangeLoading');
+            range.innerText = `${current} из ${all}`;
+            range.style.width = `${100 / all * current}%`;
+        } else {
+            const blockLoading = document.createElement("div");
+            blockLoading.id = 'loadingGames';
+            blockLoading.style.backgroundColor = 'silver';
+            blockLoading.style.height = '1.5rem';
+            blockLoading.style.width = `100%`;
+            blockLoading.style.position = 'relative';
+            blockLoading.style.textAlign = 'center';
+            blockLoading.style.color = 'white';
+            blockLoading.style.fontSize = '1 rem';
+            blockLoading.style.lineHeight = '1.5 rem';
+            const rangeLoading = document.createElement("div");
+            rangeLoading.id = 'rangeLoading';
+            rangeLoading.style.backgroundColor = 'green';
+            rangeLoading.style.position = 'absolute';
+            rangeLoading.style.top = '0';
+            rangeLoading.style.left = '0';
+            rangeLoading.style.height = '1.5rem';
+            rangeLoading.style.width = `0%`;
+            blockLoading.append(rangeLoading);
+            document.body.append(blockLoading);
+        }
+    }
 }
 
-getData.categories();
+getData.games();
+
+// небольшая функция для вызова данных в консоль при клике по ссылке
+document.querySelectorAll('a').forEach((element) => {
+    element.onclick = async (event: any) => {
+        event.preventDefault();
+        getData.makeRequest(event.target.pathname, 1).then((response: PromiseResponse) => {
+            getData.displayInformation(response);
+        });
+    }
+});
